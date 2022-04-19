c26798b4a726838d7addb0759d4d1c2c0e9a2785
elogeek <elodiechristin@gmail.com>
2022-04-19 20:06:02 +0200

    suite course

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,22 @@ const express = require('express');
 const expressApp = express();
 const server = http.createServer(expressApp);
 
-expressApp.use((req,res) => {
-    res.send("hello, request reçue");
+// First middleware (s'occupe de logger les requêtes entrantes par les users)
+expressApp.use((req,res,next) => {
+    console.log("request reçue => " + req.url)
+    next();
 });
 
-expressApp('port', process.env.PORT || 3000);
+// Second middleware (envoye une response au client)
+expressApp.use((req, res) => {
+    res.send("Hello word ! ");
+});
+
+// Three middleware (exécute un log une fois la requête finish)
+expressApp.use((req, res) => {
+    console.log("Code response: " + res.statusCode);
+    console.log("Request finish, response send user")
+});
+
+expressApp.set('port', process.env.PORT || 3000);
 server.listen(process.env.PORT || 3000, () => console.log("Server started"));
